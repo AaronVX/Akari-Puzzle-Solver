7d752d2399aaaaf7bc4c60dc414d98b24190f3c0
YanLam Ng, Ian <ngyl@myumanitoba.ca>
2020-01-19 16:33:47 -0600

    Merge branch 'master' of C:\Users\Ian\PycharmProjects\comp4190a1 with conflicts.

--- Code/AkariPuzzle.py
+++ Code/AkariPuzzle.py
@@ -1,11 +1,10 @@
 import numpy as np
-
-
-# puzzle cell value
+#puzzle cell value
 # 0-4 = wall
 # 5 = no light
 # 6 = light up
 # 7 = light bulb
+
 
 
 class AkariPuzzle:
@@ -20,9 +19,9 @@ class AkariPuzzle:
         self.original_puzzle = puzzle_arr
         self.arr = puzzle_arr.copy()
 
-    # check the position whether is on the pazzle
+    #check the position whether is on the pazzle
     def isInBounds(self, row, col):
-        if col >= 0 and col < self.cols and row >= 0 and row < self.rows:
+        if col >= 0 and col < self.cols and row >= 0 and row < self.rows :
             return True
         return False
 
@@ -31,20 +30,22 @@ class AkariPuzzle:
             return True
         return False
 
+
     def isLightBulb(self, row, col):
-        if self.arr[row, col] == AkariPuzzle.LIGHT_BULB:
+        if self.arr[row,col] == AkariPuzzle.LIGHT_BULB:
             return True
         return False
 
-    def removeLightBult(self, row, col):
+    def removeLightBult(self,row, col):
         if self.isInBounds(row, col) and self.isLightBulb(row, col):
-            self.arr[row, col] = AkariPuzzle.LIGHT_OFF
+            self.arr[row,col] = AkariPuzzle.LIGHT_OFF
             self.update_puzzle()
-            return True  # remove successfully
-        return False  # failure
+            return True         #remove successfully
+        return False            #failure
+
 
     def insert_light_bulb(self, row, col):
-        if self.isInBounds(row, col) and self.isWall(row, col) is False:
+        if self.isInBounds(row, col) and self.isWall(row,col) is False:
             self.arr[row, col] = AkariPuzzle.LIGHT_BULB
 
             for x, y in AkariPuzzle.LIGHT_DIRECTION:
@@ -56,19 +57,20 @@ class AkariPuzzle:
                         self.arr[row_temp, col_temp] = AkariPuzzle.LIGHT_ON
 
                     col_temp, row_temp = col_temp + x, row_temp + y
-            return True  # insert successfully
+            return True         #insert successfully
         return False
 
-    # return the number of light bulb neigbour cell(up, down, left, right)
+
+    #return the number of light bulb neigbour cell(up, down, left, right)
     def countNeigbourBulb(self, row, col):
         counter = 0
         for x, y in AkariPuzzle.LIGHT_DIRECTION:
-            if self.isInBounds(row + y, col + x):
-                if self.arr[row + y, col + x] == AkariPuzzle.LIGHT_BULB:
-                    counter += 1
+            if self.isInBounds(row+y, col+x):
+                 if self.arr[row+y, col+x] == AkariPuzzle.LIGHT_BULB:
+                    counter+=1
         return counter
 
-    # check there is no double light bulb in same row or same col
+    #check there is no double light bulb in same row or same col
     def isValidBulb(self, row, col):
 
         for x, y in AkariPuzzle.LIGHT_DIRECTION:
@@ -81,28 +83,20 @@ class AkariPuzzle:
                 col_temp, row_temp = col_temp + x, row_temp + y
         return False
 
-    # check whether the wall has correct number of neigbouring bulbs
-    def isWallNeigbourValid(self, row, col):
-        for x, y in AkariPuzzle.LIGHT_DIRECTION:
-            if self.isInBounds(row + y, col + x):
-                if self.arr[row + y, col + x] <= 4:
-                    if self.arr[row + y, col + x] != self.countNeigbourBulb(row + y, col + x):
-                        return False
-        return True
-
-    # update the light off cell after removing a lightbulb cell
+    #update the light off cell after removing a lightbulb cell
     def update_puzzle(self):
         bulb_indexes = np.where(self.arr == AkariPuzzle.LIGHT_BULB)
         self.arr = self.original_puzzle.copy()
-        for row, col in zip(bulb_indexes[0], bulb_indexes[1]):
+        for row, col in zip(bulb_indexes[0],bulb_indexes[1]):
             self.insert_light_bulb(row, col)
 
-    # whether the win condition is match
+
+    #whether the win condition is match
     def isFinished(self):
         isAllLightOn = False
         isWallNeigbourValid = True
         isNoDoubleBulb = True
-        if len(np.where(self.arr == AkariPuzzle.LIGHT_OFF)[0]) == 0:  # no light OFF cell
+        if len(np.where(self.arr == AkariPuzzle.LIGHT_OFF)[0]) == 0: #no light OFF cell
             isAllLightOn = True
 
         wall_indexes = np.where(self.arr <= 4)
@@ -119,6 +113,7 @@ class AkariPuzzle:
 
         return isAllLightOn and isWallNeigbourValid and isNoDoubleBulb
 
+
     def to_s(self):
         str = ''
         for i in range(self.rows):
@@ -134,16 +129,17 @@ class AkariPuzzle:
                     str += 'b'
             str += '\n'
 
+
     def print_puzzle(self):
-        for i in range(self.rows):
+        for i in range(self.rows) :
             for j in range(self.cols):
-                value = self.arr[i, j]
+                value = self.arr[i,j]
                 if value in range(5):
-                    print(self.arr[i][j], end=' ')
+                    print(self.arr[i][j], end = ' ')
                 elif value == AkariPuzzle.LIGHT_OFF:
-                    print('_', end=' ')
+                    print('_', end = ' ')
                 elif value == AkariPuzzle.LIGHT_ON:
-                    print('#', end=' ')
+                    print('#', end = ' ')
                 elif value == AkariPuzzle.LIGHT_BULB:
-                    print('b', end=' ')
+                    print('b', end = ' ')
             print()
